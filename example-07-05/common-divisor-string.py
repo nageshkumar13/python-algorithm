eb07eae3db91ac6688b28162aab48a01feaa2fb2
nageshkumar13 <nageshkumar1925@gmail.com>
2024-06-17 14:33:43 +0530

    added files

--- example-07-05/common-divisor-string.py
+++ example-07-05/common-divisor-string.py
@@ -1,18 +1,18 @@
 '''
-For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times)
-
+For strings S and T, we say "T divides S" if and only if S = T + ... + T 
+(T concatenated with itself 1 or more times)
 Return the largest string X such that X divides str1 and X divides str2.
-
- 
 
 Example 1:
 
 Input: str1 = "ABCABC", str2 = "ABC"
 Output: "ABC"
+
 Example 2:
 
 Input: str1 = "ABABAB", str2 = "ABAB"
 Output: "AB"
+
 Example 3:
 
 Input: str1 = "LEET", str2 = "CODE"
@@ -52,4 +52,4 @@ class Solution(object):
         # If no common divisor found, return empty string
 
 apt = Solution()
-print(apt.gcdOfStrings(str1 = "ABCABC", str2 = "ABC"))
+print(apt.gcdOfStrings(str1 = "AAAAA", str2 = "AA"))
